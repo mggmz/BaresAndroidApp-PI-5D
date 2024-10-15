e96ebc663c96a36738127838becf400dbe2d67de
Axel Sandoval <axesan917@gmail.com>
2024-10-15 16:23:52 -0600

    rebase process

--- YUMEAT/app/src/main/java/com/axldev/yumeat/MainActivity.kt
+++ YUMEAT/app/src/main/java/com/axldev/yumeat/MainActivity.kt
@@ -8,9 +8,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OnboardingScreen(
-
-            ) // Llamada a la función de la pantalla de onboarding
+            OnboardingScreen() // Llamada a la función de la pantalla de onboarding
         }
     }
 }
