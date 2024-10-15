f8d991725b28683550ba459be4b7fc0baad18d98
Axel Sandoval <axesan917@gmail.com>
2024-10-15 16:13:19 -0600

    dev branch save

--- YUMEAT/app/src/main/java/com/axldev/yumeat/MainActivity.kt
+++ YUMEAT/app/src/main/java/com/axldev/yumeat/MainActivity.kt
@@ -8,7 +8,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OnboardingScreen() // Llamada a la función de la pantalla de onboarding
+            OnboardingScreen(
+
+            ) // Llamada a la función de la pantalla de onboarding
         }
     }
 }
