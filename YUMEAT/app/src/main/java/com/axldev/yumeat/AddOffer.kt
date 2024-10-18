4e23520f1f20e6521290ac80d6db4c63cafbf6ad
Axel Sandoval <axesan917@gmail.com>
2024-10-18 12:27:18 -0600

    add event data picker

--- YUMEAT/app/src/main/java/com/axldev/yumeat/AddOffer.kt
+++ YUMEAT/app/src/main/java/com/axldev/yumeat/AddOffer.kt
@@ -1,5 +1,6 @@
 package com.axldev.yumeat
 
+import android.widget.DatePicker
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,21 +19,35 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.auth.FirebaseAuth
 import android.widget.Toast
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.draw.clip
+import java.util.*
 
 @Composable
 fun AddOfferScreen(
     onOfferAdded: () -> Unit,
-    onNavigateToHome: () -> Unit,  // Parámetro para navegar a Home
-    onNavigateToOffers: () -> Unit // Parámetro para navegar a la pantalla de ofertas
+    onNavigateToHome: () -> Unit,
+    onNavigateToOffers: () -> Unit
 ) {
     var offerName by remember { mutableStateOf("") }
     var offerDetails by remember { mutableStateOf("") }
+    var offerDate by remember { mutableStateOf("") }
     val context = LocalContext.current
     val db = FirebaseFirestore.getInstance()
     val auth = FirebaseAuth.getInstance()
 
     val currentUser = auth.currentUser
+
+    // Instancia del calendario para DatePicker
+    val calendar = Calendar.getInstance()
+    var selectedYear by remember { mutableStateOf(calendar.get(Calendar.YEAR)) }
+    var selectedMonth by remember { mutableStateOf(calendar.get(Calendar.MONTH)) }
+    var selectedDay by remember { mutableStateOf(calendar.get(Calendar.DAY_OF_MONTH)) }
+
+    // Función para actualizar la fecha seleccionada
+    fun updateSelectedDate(year: Int, month: Int, day: Int) {
+        offerDate = "$day/${month + 1}/$year"
+    }
 
     Scaffold(
         bottomBar = {
@@ -51,13 +66,13 @@ fun AddOfferScreen(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     IconButton(
-                        onClick = onNavigateToHome,  // Navega al Home cuando se presiona el botón
+                        onClick = onNavigateToHome,
                         modifier = Modifier.size(48.dp)
                     ) {
                         Icon(Icons.Filled.Home, contentDescription = "Home", tint = Color.Gray)
                     }
                     IconButton(
-                        onClick = onNavigateToOffers,  // Navega a la pantalla de ofertas
+                        onClick = onNavigateToOffers,
                         modifier = Modifier.size(48.dp)
                     ) {
                         Icon(Icons.Filled.LocalOffer, contentDescription = "Offers", tint = Color.Gray)
@@ -66,57 +81,78 @@ fun AddOfferScreen(
             }
         }
     ) { innerPadding ->
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .padding(16.dp)
                 .background(Color.White)
         ) {
-            Column(
+            Text(
+                text = "Add Offer",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "Add Offer",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .padding(bottom = 12.dp, top = 28.dp)
-                        .align(Alignment.CenterHorizontally)
-                )
+                    .padding(bottom = 12.dp, top = 24.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
 
-                // Offer Name Field
-                OutlinedTextField(
-                    value = offerName,
-                    onValueChange = { offerName = it },
-                    label = { Text("Offer Name") },
-                    modifier = Modifier.fillMaxWidth()
-                )
+            // Offer Name Field
+            OutlinedTextField(
+                value = offerName,
+                onValueChange = { offerName = it },
+                label = { Text("Offer Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
 
-                Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-                // Offer Details Field
-                OutlinedTextField(
-                    value = offerDetails,
-                    onValueChange = { offerDetails = it },
-                    label = { Text("Offer Details") },
-                    modifier = Modifier.fillMaxWidth()
-                )
+            // Offer Details Field
+            OutlinedTextField(
+                value = offerDetails,
+                onValueChange = { offerDetails = it },
+                label = { Text("Offer Details") },
+                modifier = Modifier.fillMaxWidth()
+            )
 
-                Spacer(modifier = Modifier.height(16.dp))
-            }
+            Spacer(modifier = Modifier.height(16.dp))
 
-            // Botón en la parte inferior de la pantalla
+            // Offer Date Field (mostrar solo el placeholder)
+            OutlinedTextField(
+                value = offerDate,
+                onValueChange = { offerDate = it },
+                label = { Text("Select Date") },
+                modifier = Modifier.fillMaxWidth(),
+                readOnly = true
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Mostrar el DatePicker fijo debajo del campo de fecha
+            DatePickerOfferView(
+                year = selectedYear,
+                month = selectedMonth,
+                day = selectedDay,
+                onDateChange = { year, month, day ->
+                    selectedYear = year
+                    selectedMonth = month
+                    selectedDay = day
+                    updateSelectedDate(year, month, day)
+                }
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Add Offer Button
             Button(
                 onClick = {
-                    if (offerName.isNotEmpty() && offerDetails.isNotEmpty()) {
+                    if (offerName.isNotEmpty() && offerDetails.isNotEmpty() && offerDate.isNotEmpty()) {
                         if (currentUser != null) {
                             val userUID = currentUser.uid
                             val offer = hashMapOf(
                                 "offerName" to offerName,
                                 "offerDetails" to offerDetails,
+                                "offerDate" to offerDate,
                                 "userUID" to userUID
                             )
                             db.collection("offers")
@@ -138,12 +174,31 @@ fun AddOfferScreen(
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0072A3)),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .align(Alignment.BottomCenter)  // Alinea el botón en la parte inferior
-                    .padding(16.dp)
                     .height(48.dp)
             ) {
                 Text(text = "Add Offer", color = Color.White)
             }
         }
     }
+}
+
+@Composable
+fun DatePickerOfferView(
+    year: Int,
+    month: Int,
+    day: Int,
+    onDateChange: (Int, Int, Int) -> Unit
+) {
+    AndroidView(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp),
+        factory = { context ->
+            DatePicker(context).apply {
+                init(year, month, day) { _, selectedYear, selectedMonth, selectedDay ->
+                    onDateChange(selectedYear, selectedMonth, selectedDay)
+                }
+            }
+        }
+    )
 }
